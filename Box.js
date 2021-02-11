e8ce7528aa2bc4f49cca971b45cd1af9fd7bb882
Lakshya Khubchandani <73833849+Lakshya5468@users.noreply.github.com>
2021-02-12 00:04:12 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,17 @@
+class Box extends BaseClass {
+  constructor(x, y, width, height){
+    super(x,y,40,40);
+    this.image=loadImage("block.png");
+  }
+  display(){
+    push();
+    if(this.body.speed<5){
+      super.display();
+    } else{
+      World.remove(world,this.body);
+      this.visibility=this.visibility-5;
+      tint(255,this.visibility);
+    }
+    pop();
+  }
+};
